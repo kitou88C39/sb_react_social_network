90dcbefea8a03140df457db992638cd13ee237d0
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-06 09:38:17 +0900

    src/main/java/condigtechniquesにcondigtechniques.modelフォルダにPost.javaを作成する

--- social_app/src/main/java/codingtechniques/condigtechniques.model/Post.java
+++ social_app/src/main/java/codingtechniques/condigtechniques.model/Post.java
@@ -1,8 +1,13 @@
 package condigtechniques.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class Post {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
