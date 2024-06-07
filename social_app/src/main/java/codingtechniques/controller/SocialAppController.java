f59d62346f5247bb6e897ce98922db8ca60101d1
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-07 20:30:35 +0900

    src/main/java/condigtechniquesにcontrollerフォルダとSocialAppController.javaを作成する

--- social_app/src/main/java/codingtechniques/controller/SocialAppController.java
+++ social_app/src/main/java/codingtechniques/controller/SocialAppController.java
@@ -1,5 +1,16 @@
 package codingtechniques.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import condigtechniques.repository.PostRepository;
+
+@RestController
+@RequestMapping("/socila/api")
 public class SocialAppController {
+
+    @Autowired
+    private PostRepository PostRepository;
 
 }
