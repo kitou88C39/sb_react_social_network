7782a4612a5f90d8cfbb37c13a01ba23c6a89b0e
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-07 19:46:55 +0900

    src/main/java/condigtechniquesにcondigtechniques.modelにComment.javaを作成する

--- social_app/src/main/java/codingtechniques/condigtechniques.model/Comment.java
+++ social_app/src/main/java/codingtechniques/condigtechniques.model/Comment.java
@@ -1,5 +1,14 @@
 package condigtechniques.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
