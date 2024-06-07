ae51c6a7d585625524169fe2f596cf0119427592
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-07 17:58:36 +0900

    src/main/java/condigtechniquesにcondigtechniques.modelフォルダにPost.javaを作成する

--- social_app/src/main/java/codingtechniques/condigtechniques.model/Post.java
+++ social_app/src/main/java/codingtechniques/condigtechniques.model/Post.java
@@ -48,4 +48,32 @@ public class Post {
         this.unlike = unlike;
         this.comments = comments;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getLike() {
+        return like;
+    }
 }
