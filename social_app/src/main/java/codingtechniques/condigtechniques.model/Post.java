e1d20d3e96227d676afe0fb7697342b625509caa
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-07 10:17:42 +0900

    src/main/java/condigtechniquesにcondigtechniques.modelフォルダにPost.javaを作成する

--- social_app/src/main/java/codingtechniques/condigtechniques.model/Post.java
+++ social_app/src/main/java/codingtechniques/condigtechniques.model/Post.java
@@ -2,6 +2,8 @@ package condigtechniques.model;
 
 import java.util.List;
 import java.util.ArrayList;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,9 +15,17 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "users")
     private Strig user;
-    private String cotent;
+
+    @Column(name = "contents")
+    private String content;
+
+    @Column(name = "post-like")
     private int like;
+
+    @Column(name = "post-unlike")
     private int unlike;
 
     List<Comment> comments = new ArrayList<>();
